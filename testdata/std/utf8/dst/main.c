c639cc092f5ec683128ffaa151776361ebf668d8
Anton <m@antonz.org>
2026-03-14 03:24:02 +0500

    testdata: std/utf8 - smoke test

--- testdata/std/utf8/dst/main.c
+++ testdata/std/utf8/dst/main.c
@@ -3,4 +3,88 @@
 // -- Implementation --
 
 int main(void) {
+    {
+        // DecodeLastRune.
+        so_Slice b = so_string_bytes(so_str("Hello, 世界"));
+        so_Result _res1 = utf8_DecodeLastRune(b);
+        so_rune r = _res1.val.as_rune;
+        so_int size = _res1.val2.as_int;
+        if (r != U'界' || size != 3) {
+            so_panic("DecodeLastRune failed");
+        }
+    }
+    {
+        // DecodeLastRuneInString.
+        so_String str = so_str("Hello, 世界");
+        so_Result _res2 = utf8_DecodeLastRuneInString(str);
+        so_rune r = _res2.val.as_rune;
+        so_int size = _res2.val2.as_int;
+        if (r != U'界' || size != 3) {
+            so_panic("DecodeLastRuneInString failed");
+        }
+    }
+    {
+        // DecodeRune.
+        so_Slice b = so_string_bytes(so_str("Hello, 世界"));
+        so_Result _res3 = utf8_DecodeRune(b);
+        so_rune r = _res3.val.as_rune;
+        so_int size = _res3.val2.as_int;
+        if (r != U'H' || size != 1) {
+            so_panic("DecodeRune failed");
+        }
+    }
+    {
+        // DecodeRuneInString.
+        so_String str = so_str("Hello, 世界");
+        so_Result _res4 = utf8_DecodeRuneInString(str);
+        so_rune r = _res4.val.as_rune;
+        so_int size = _res4.val2.as_int;
+        if (r != U'H' || size != 1) {
+            so_panic("DecodeRuneInString failed");
+        }
+    }
+    {
+        // EncodeRune.
+        so_Slice buf = so_make_slice(so_byte, 3, 3);
+        so_int n = utf8_EncodeRune(buf, U'界');
+        if (n != 3 || so_string_ne(so_bytes_string(buf), so_str("界"))) {
+            so_panic("EncodeRune failed");
+        }
+    }
+    {
+        // RuneCount.
+        so_int n = utf8_RuneCount(so_string_bytes(so_str("Hello, 世界")));
+        if (n != 9) {
+            so_panic("RuneCount failed");
+        }
+    }
+    {
+        // RuneCountInString.
+        so_int n = utf8_RuneCountInString(so_str("Hello, 世界"));
+        if (n != 9) {
+            so_panic("RuneCountInString failed");
+        }
+    }
+    {
+        // RuneLen.
+        so_int n = utf8_RuneLen(U'界');
+        if (n != 3) {
+            so_panic("RuneLen failed");
+        }
+    }
+    {
+        // ValidString.
+        if (!utf8_ValidString(so_str("Hello, 世界"))) {
+            so_panic("ValidString failed");
+        }
+    }
+    {
+        // AppendRune.
+        so_Slice buf = so_make_slice(so_byte, 7, 10);
+        so_copy(so_byte, buf, so_string_bytes(so_str("Hello, ")));
+        buf = utf8_AppendRune(buf, U'界');
+        if (so_string_ne(so_bytes_string(buf), so_str("Hello, 界"))) {
+            so_panic("AppendRune failed");
+        }
+    }
 }
--- testdata/std/utf8/src/main.go
+++ testdata/std/utf8/src/main.go
@@ -3,5 +3,80 @@ package main
 import "github.com/nalgeon/solod/so/unicode/utf8"
 
 func main() {
-	var _ = utf8.MaxRune
+	{
+		// DecodeLastRune.
+		b := []byte("Hello, 世界")
+		r, size := utf8.DecodeLastRune(b)
+		if r != '界' || size != 3 {
+			panic("DecodeLastRune failed")
+		}
+	}
+	{
+		// DecodeLastRuneInString.
+		str := "Hello, 世界"
+		r, size := utf8.DecodeLastRuneInString(str)
+		if r != '界' || size != 3 {
+			panic("DecodeLastRuneInString failed")
+		}
+	}
+	{
+		// DecodeRune.
+		b := []byte("Hello, 世界")
+		r, size := utf8.DecodeRune(b)
+		if r != 'H' || size != 1 {
+			panic("DecodeRune failed")
+		}
+	}
+	{
+		// DecodeRuneInString.
+		str := "Hello, 世界"
+		r, size := utf8.DecodeRuneInString(str)
+		if r != 'H' || size != 1 {
+			panic("DecodeRuneInString failed")
+		}
+	}
+	{
+		// EncodeRune.
+		buf := make([]byte, 3)
+		n := utf8.EncodeRune(buf, '界')
+		if n != 3 || string(buf) != "界" {
+			panic("EncodeRune failed")
+		}
+	}
+	{
+		// RuneCount.
+		n := utf8.RuneCount([]byte("Hello, 世界"))
+		if n != 9 {
+			panic("RuneCount failed")
+		}
+	}
+	{
+		// RuneCountInString.
+		n := utf8.RuneCountInString("Hello, 世界")
+		if n != 9 {
+			panic("RuneCountInString failed")
+		}
+	}
+	{
+		// RuneLen.
+		n := utf8.RuneLen('界')
+		if n != 3 {
+			panic("RuneLen failed")
+		}
+	}
+	{
+		// ValidString.
+		if !utf8.ValidString("Hello, 世界") {
+			panic("ValidString failed")
+		}
+	}
+	{
+		// AppendRune.
+		buf := make([]byte, 7, 10)
+		copy(buf, []byte("Hello, "))
+		buf = utf8.AppendRune(buf, '界')
+		if string(buf) != "Hello, 界" {
+			panic("AppendRune failed")
+		}
+	}
 }
